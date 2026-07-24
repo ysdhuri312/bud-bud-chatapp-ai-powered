950f40bf8c2fb3a5344b787df269bf0d5befbd8b
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-07-24 17:56:23 +0530

    Add footer

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className=''>
+      <div className='container mx-auto'>
+        <div className='flex justify-between py-3'>
+          <p>Footer</p>
+          <div></div>
+        </div>
+      </div>
+    </footer>
+  );
+}
