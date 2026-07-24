b7f40b82cd264125ce62788ff3ddf0360e8a34db
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-07-24 14:36:08 +0530

    Install and setup react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/layout/Header';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 function App() {
   return (
     <>
       <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
 import { ClerkProvider } from '@clerk/react';
+import './index.css';
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
