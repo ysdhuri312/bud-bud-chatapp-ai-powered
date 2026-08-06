cb4dda075a68e8852100c87c51285d2d48f9ae27
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-08-06 14:52:12 +0530

    Export all methods

--- backend/src/module/user/container.ts
+++ backend/src/module/user/container.ts
@@ -2,6 +2,6 @@ import { UserController } from './user.controller.js';
 import { UserRepository } from './user.repository.js';
 import { UserService } from './user.service.js';
 
-const userRepository = new UserRepository();
-const userService = new UserService(userRepository);
-export const useController = new UserController(userService);
+export const userRepository = new UserRepository();
+export const userService = new UserService(userRepository);
+export const userController = new UserController(userService);
