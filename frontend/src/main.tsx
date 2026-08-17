8421b45e0288aa21637dd8bacb551ecebf9fa0e0
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-08-17 11:56:32 +0530

    Remove strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,8 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
 import { router } from './app/router.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 );
