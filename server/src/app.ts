b38b7d759283bf22a06cc28d945f9cc7096bcfca
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-08-30 20:06:06 +0530

    Create register route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,24 @@
 import http from 'node:http';
 import express from 'express';
 import { errorMiddleware } from './handlers/error.handler.js';
-import { AppError } from './handlers/AppError.js';
-import { configDotenv } from 'dotenv';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth/register.route.js';
 
 export const app = express();
 export const server = http.createServer(app);
 
-configDotenv();
+dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (_req, res, next) => {
-  return next(new AppError(400, 'Custome error'));
-
+app.get('/', (_req, res) => {
   res.status(200).json({
     success: true,
     message: 'Welcome to API v1.0.0',
     timestamp: new Date().toISOString(),
   });
 });
+
+app.use('/api/v1/auth', authRoutes);
 
 app.use(errorMiddleware);
