e4d5f82d961a6486f4a773c8151b44b079abe8e8
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-09-03 11:46:28 +0530

    Setup react router for routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <Navbar />
+      ChatAPP
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}></Route>
+    </Routes>
+  </BrowserRouter>,
+);
